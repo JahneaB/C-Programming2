7bc67a216d570af97ff703661649f5818efd951b
Jahnea Bush <jbush0811@floridapoly.edu>
2026-01-16 19:12:55 +0000

    First day

---
+++ 01_first_program/first_program.cpp
@@ -0,0 +1,24 @@
+// ctrl j hide explorer
+/*
+    First c++ prgram 
+    01/14/26
+*/
+
+#include <iostream> //input output stream
+
+
+
+int main(void){
+    //print to the cousole
+
+    std::cout <<"hello from COP3337\n" << std :: endl;
+    // std is standard namespace 
+    //:: scope resolution 
+    //cout is the cousole output
+    // << stream insertion operator 
+    // endl - end of line
+    std :: cout << "45 * 98 -90 = "<< 45 * 98 -90 << std :: endl;
+
+    
+    return 0; // 0 success non 0 error
+}
