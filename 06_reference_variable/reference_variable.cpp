abc7d21ad2273d094dcfe0802f0fe60391d8c8c0
Jahnea Bush <jbush0811@floridapoly.edu>
2026-03-28 17:10:09 +0000

    ref variable

---
+++ 06_reference_variable/reference_variable.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void add_five(int& num);
+
+int main(void){
+    int a = 10; 
+    // createa reference variable
+    int& a_ref = a;
+    a+=10;
+    std:: cout<< a << " "<< a_ref<< std::endl;
+    
+    a_ref *= 10;
+    std:: cout<< a << " "<< a_ref<< std::endl;
+    
+    add_five(a);
+    std:: cout<< a << " "<< a_ref<< std::endl;
+    
+    return 0;
+}
+void add_five(int& num){
+    num += 5;
+
+}
